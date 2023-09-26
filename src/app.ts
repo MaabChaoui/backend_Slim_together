1ba3e5ad29aaf4c85c0ea2ec18f8025319e110af
maabChaoui <maab.chaoui@ensia.edu.dz>
2023-09-26 19:31:15 +0200

    initial setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-require('dotenv').config();
-import express, { Response } from 'express';
-import config from 'config';
-import validateEnv from './utils/validateEnv';
-import { AppDataSource } from './utils/data-source';
+require("dotenv").config();
+import express, { Response } from "express";
+import config from "config";
+import validateEnv from "./utils/validateEnv";
+import { AppDataSource } from "./utils/data-source";
+import bodyParser from "body-parser";
+import cors from "cors";
+import router from "./routes/routes.index";
 //import redisClient from './utils/connectRedis';
 
 AppDataSource.initialize()
@@ -11,8 +14,9 @@ AppDataSource.initialize()
     validateEnv();
 
     const app = express();
-
+    app.use('/api/', router)
     // MIDDLEWARE
+    app.use(bodyParser.json())
 
     // 1. Body parser
 
@@ -21,26 +25,25 @@ AppDataSource.initialize()
     // 3. Cookie Parser
 
     // 4. Cors
-
+    //app.use(cors)
     // ROUTES
 
     // HEALTH CHECKER
-    app.get('/api/healthchecker', async (_, res: Response) => {
-        const message = "here we go"
-        res.status(200).json({
-          status: 'success',
-          message,
-        });
+    app.get("/api/healthchecker", async (_, res: Response) => {
+      const message = "here we go";
+      res.status(200).json({
+        status: "success",
+        message,
       });
-  
-      // UNHANDLED ROUTE
-  
-      // GLOBAL ERROR HANDLER
-  
-      const port = config.get<number>('port');
-      app.listen(port);
-  
-      console.log(`Server started on port: ${port}`);
-    })
-    .catch((error) => console.log(error));
-  
+    });
+
+    // UNHANDLED ROUTE
+
+    // GLOBAL ERROR HANDLER
+
+    const port = config.get<number>("port");
+    app.listen(port);
+
+    console.log(`Server started on port: ${port}`);
+  })
+  .catch((error) => console.log(error));
